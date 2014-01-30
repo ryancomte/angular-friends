377a076adfa4ef3b7726bb949e89c6237d77332d
Ryan Comte <ryancomte@gmail.com>
2014-01-29 23:05:04 -0700

    Added functionality to friends page.

--- FriendController.js
+++ FriendController.js
@@ -1,3 +1,16 @@
+app.controller('friendsCtrl', function($scope, $http){
+    $http.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json').success(function(data){
+        $scope.friends = data.results;
+        angular.forEach($scope.friends, function(value, key){
+            value.friend_count = parseInt(value.friend_count);
+        });
+
+        $scope.searchTerm = null;
+        $scope.sortingVar = 'name';
+        $scope.ascDesc = true;
+
+    });
+});
 
 
 
